120871001d5afdc9597ef383896e53058b55d1af
chase <chaselinzmeyer@gmail.com>
2019-04-06 09:01:20 -0500

    imported view1 | created route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route} from 'react-router-dom';
 //import axios from 'axios';
 
-import View1 from '../View1/View1';
+import Admin from '../Admin/Admin';
 import Home from '../Home/Home';
-
+import View1 from '../View1/View1';
 
 
 class App extends Component {
   render() {
     return (
       <Router>
+      	<Route exact path='/admin' component={Admin} />
 				<Route exact path='/' component={Home} />
-      	<Route exact path='/view-1' component={View1} />
+      	<Route exact path='/view1' component={View1} />
+
       </Router>
     );
   }
