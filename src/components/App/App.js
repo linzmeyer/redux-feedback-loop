3950ce04d8c7c4dc1d10cc6d9adc134cb7300555
chase <chaselinzmeyer@gmail.com>
2019-04-06 07:45:10 -0500

    added react routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import { HashRouter as Router, Route} from 'react-router-dom';
+//import axios from 'axios';
+
+import View1 from '../View1/View1';
+//import Home from '../Home/Home';
+
+//import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      	<Route />
-      	<Route />
-      	<Route />
-      	<Route />
-      </div>
+      <Router>
+
+      	<Route exact path='/view-1' component={View1} />
+      </Router>
     );
   }
 }
