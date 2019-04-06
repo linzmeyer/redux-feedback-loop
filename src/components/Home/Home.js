465ed96492c9676cb82ccf2a95fccce3e8a4e8fb
chase <chaselinzmeyer@gmail.com>
2019-04-06 09:02:31 -0500

    created navs to route to view1 and admin

---
+++ src/components/Home/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+	linkToView1 = () => {
+		alert( 'changing to view1' );
+		console.log(this.props);
+		this.props.history.push('/view1')
+		console.log(this.props);
+	}
+
+	linkToAdmin = () => {
+		this.props.history.push('/admin')
+	}
+
+  render() {
+    return (
+			<div>
+				<nav>
+					<div className='navbar' onClick={this.linkToView1} >Start Feedback</div>
+					<div className='navbar' onClick={this.linkToAdmin} >Admin</div>
+				</nav>
+			</div>
+    );
+  }
+}
+
+export default(Home);
