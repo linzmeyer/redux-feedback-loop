c28fe55c9ea9a8c26b8bb192693b03858724fd2c
chase <chaselinzmeyer@gmail.com>
2019-04-06 07:46:45 -0500

    added home route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,15 @@ import { HashRouter as Router, Route} from 'react-router-dom';
 //import axios from 'axios';
 
 import View1 from '../View1/View1';
-//import Home from '../Home/Home';
+import Home from '../Home/Home';
 
-//import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-
+				<Route exact path='/' component={Home} />
       	<Route exact path='/view-1' component={View1} />
       </Router>
     );
