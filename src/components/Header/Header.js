75dc3a07ae9772a387d9d4fed8896cd2da7d5412
chase <chaselinzmeyer@gmail.com>
2019-04-06 20:30:05 -0500

    created conditional rendering using props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './Header.css';
 
+// Parents: <View1-5 />
 class Header extends Component {
+
+	renderHeader = () => {
+		if ( this.props.lastPage === true ) {
+			return ( <h1>Feedback!</h1> );
+		}
+		else {
+			return (
+				<div>
+					<h1>Feedback!</h1>
+					<h4><i>Don't forget it!</i></h4>
+				</div>
+			);
+		}
+	}
+
   render() {
     return (
-			<div>
-        <header className="Header">
-          <h1>Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-      </div>
+			<header className="Header" >
+				{ this.renderHeader() }
+			</header>
     );
   }
 }
