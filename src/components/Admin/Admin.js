4d79c7e78e46558e97602116a4d868be81fa9448
chase <chaselinzmeyer@gmail.com>
2019-04-07 18:56:47 -0500

    began table skeleton

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Header from '../Header/Header';
 
 
@@ -6,19 +8,45 @@ import Header from '../Header/Header';
 
 class Admin extends Component {
 
+	componentDidMount = () => {
+		this.props.getAllFeedback();
+	}
 
-componentDidMount = () => {
-	this.props.getAllFeedback();
-}
+	renderTableRow = () => {
+		console.log(this.props.reduxState.allFeedback);
+		return(
+			<tr>
+
+			</tr>
+		);
+	}
 
   render() {
     return (
 			<div>
 				<Header currentView="admin" />
 				<h2>HELLO ADMIN</h2>
+				<table>
+					<thead>
+						<tr>
+							<th>Feeling</th>
+							<th>Understanding</th>
+							<th>Support</th>
+							<th>Comments</th>
+							<th>Date</th>
+							<th>Flag For Further Review</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody id="viewKoalas">
+						{ this.renderTableRow() }
+					</tbody>
+				</table>
 			</div>
     );
   }
 }
 
-export default(Admin);
+const mapReduxStateToProps = reduxState => ({ reduxState });
+
+export default connect( mapReduxStateToProps )( Admin );
