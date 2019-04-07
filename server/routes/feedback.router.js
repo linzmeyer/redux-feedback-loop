34b136d3c8a26c597a89487a7fc43cdbef24529c
chase <chaselinzmeyer@gmail.com>
2019-04-07 12:36:34 -0500

    init, set up POST request to DB

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,33 @@
+const express = require( 'express' );
+const router = express.Router();
+const pool = require( '../modules/pool' );
+
+//router.get('/', ( req, res ) => {
+//    console.log('GET /api/pizza');
+//    pool.query('SELECT * from "pizza";').then((result) => {
+//        res.send(result.rows);
+//    }).catch((error) => {
+//        console.log('Error GET /api/pizza', error)
+//        res.sendStatus(500);
+//    });
+//})
+
+// POST
+// Add a review to the database
+router.post('/', (req, res) => {
+  let feedback = req.body;
+  console.log( 'Adding feedback', feedback );
+	let sqlText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);`;
+  pool.query(sqlText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comment])
+    .then( (response) => {
+      res.sendStatus(201);
+    })
+    .catch( (error) => {
+      console.log('Failed to insert new feedback', feedback);
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
+
+
+module.exports = router;
