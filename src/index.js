1a65e76bec6bbe18bf34a59299f6c70914ef45e1
chase <chaselinzmeyer@gmail.com>
2019-04-06 19:31:31 -0500

    created first 3 views reducers

--- src/index.js
+++ src/index.js
@@ -3,16 +3,39 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
 
 import './index.css';
 import App from './components/App/App';
-import { logger } from 'redux-logger';
 
-// This will store our reducers within the global scope of <Provider />
+const feeling = ( state = 'unanswered', action ) => {
+	if ( action.type === 'SET_FEELING' ) {
+		return action.payload;
+	}
+	return state;
+}
+
+const understanding = ( state = 'unanswered', action ) => {
+	if ( action.type === 'SET_UNDERSTANDING' ) {
+		return action.payload;
+	}
+	return state;
+}
+
+const support = ( state = 'unanswered', action ) => {
+	if ( action.type === 'SET_SUPPORT' ) {
+		return action.payload;
+	}
+	return state;
+}
+
+// This stores our reducers within the global scope of <Provider />
 const storeInstance = createStore(
-	// This puts all of our separate reducers into one object.
+	// This puts all individual reducers into one object.
 	combineReducers({
-
+		feeling,
+		understanding,
+		support,
 	}),
 	// This gives us the ability to track dispatches in client console.
 	applyMiddleware(logger),
